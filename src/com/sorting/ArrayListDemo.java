8f3406f3b7802334ab46ec3d81cb15d873e89292
kru-nal <krunaal.java@gmail.com>
2025-05-08 18:23:23 +0530

    Data contain

---
+++ src/com/sorting/ArrayListDemo.java
@@ -0,0 +1,20 @@
+package com.sorting;
+import java.util.ArrayList;
+public class ArrayListDemo {
+	
+	// sort element using ArraYlist.
+	
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList();
+		list.add("krunal");
+		list.add(101);
+		list.add("Patil");
+		list.add("pune");
+		
+		
+		System.out.println(list.get(2));
+		
+		
+	}
+
+}
---
+++ src/com/sorting/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package com.sorting;
+import java.util.LinkedList;
+public class LinkedListDemo {
+public static void main(String[] args) {
+	
+	
+	LinkedList <String> list = new LinkedList<String>();
+	
+	list.add("India");
+	list.add("Russia");
+	list.add("Usa");
+	list.add("Israel");
+	for (String A:list) {
+		System.out.println(A);
+	}
+}
+}
